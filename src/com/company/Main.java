b9cead75c92366ed4d3bba3333c0ed51582bc281
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-10-29 11:31:34 +0530

    added changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,20 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Product appleMacbookAir=new Product();
-        appleMacbookAir.id=1;
-        appleMacbookAir.name="Apple Macbook Air";
-        appleMacbookAir.category="Laptop";
-        appleMacbookAir.salesPrice=80000f;
-        appleMacbookAir.cost=60000f;
-        appleMacbookAir.quantity=1000;
-
-        appleMacbookAir.activate();
-        System.out.println(appleMacbookAir.name);
-        System.out.println(appleMacbookAir.active);
-        appleMacbookAir.deactivate();
-        System.out.println(appleMacbookAir.active);
-        System.out.println(appleMacbookAir.getProfitOrLoss());
+//        Product appleMacbookAir=new Product();
+//        appleMacbookAir.id=1;
+//        appleMacbookAir.name="Apple Macbook Air";
+//        appleMacbookAir.category="Laptop";
+//        appleMacbookAir.salesPrice=80000f;
+//        appleMacbookAir.cost=60000f;
+//        appleMacbookAir.quantity=1000;
+//
+//        appleMacbookAir.activate();
+//        System.out.println(appleMacbookAir.name);
+//        System.out.println(appleMacbookAir.active);
+//        appleMacbookAir.deactivate();
+//        System.out.println(appleMacbookAir.active);
+//        System.out.println(appleMacbookAir.getProfitOrLoss());
 
         Customer customer = new Customer();
 
@@ -31,6 +31,36 @@ public class Main {
         System.out.println(customer.contactName);
         System.out.println(customer.getAddressDetails());
         System.out.println(customer.calculateDiscount());
+        System.out.println("Address ends");
+        System.out.println();
+
+        Product appleMacBookAir =
+                new Product(1, "Apple MacBook Air", "Laptop",
+                        80000f, 60000f, 1000, true);
+
+        System.out.println(appleMacBookAir.name);
+        System.out.println(appleMacBookAir.active);
+        appleMacBookAir.deactivate();
+        System.out.println(appleMacBookAir.active);
+        System.out.println(appleMacBookAir.getProfitOrLoss());
+        System.out.println("macbook ends");
+        System.out.println();
+
+        //implementing copy constructor
+        Product p2=new Product(appleMacBookAir);
+        p2.id=3;
+        p2.name="Lenovo";
+        System.out.println(p2.name);
+        System.out.println(p2.active);
+        p2.deactivate();
+        System.out.println(p2.active);
+        System.out.println(p2.getProfitOrLoss());
+        System.out.println("P2 ends");
+        System.out.println();
+
+
+        Product hillary=new Product();
+        System.out.println(hillary.name);
 
     }
 }
--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -9,19 +9,92 @@ public class Product {
     int quantity;
     boolean active;
 
+//    Product () {
+//        System.out.println("Created Product Object");
+//        id = 1;
+//        name = "default name";
+//        category = "default category";
+//    }
+
+    //parameterized constructor
+//    Product (int _id, String _name, String _category,
+//             float _salesPrice, float _cost, int _quantity,
+//             boolean _active) {
+//        id = _id;
+//        name = _name;
+//        category = _category;
+//        salesPrice = _salesPrice;
+//        cost = _cost;
+//        quantity = _quantity;
+//        active = _active;
+//    }
+//
+//
+//    //copy constructor
+//    Product (Product product) {
+//        id = product.id;
+//        name = product.name;
+//        category = product.category;
+//        salesPrice = product.salesPrice;
+//        cost = product.cost;
+//        quantity = product.quantity;
+//        active = product.active;
+//    }
+//
+//    float getProfitOrLoss() {
+//        return salesPrice - cost;
+//    }
+//
+//    void activate() {
+//        active = true;
+//    }
+//
+//    void deactivate() {
+//        active = false;
+//    }
+//
+//    boolean isBelowThreshold() {
+//        return quantity < 100;
+//    }
+    Product() {
+        this(1, "product", "category",
+                0f, 0f, 0, false);
+    }
+    Product (int id, String name, String category,
+             float salesPrice, float cost, int quantity,
+             boolean active) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.salesPrice = salesPrice;
+        this.cost = cost;
+        this.quantity = quantity;
+        this.active = active;
+    }
+
+    Product (Product product) {
+        this.id = product.id;
+        this.name = product.name;
+        this.category = product.category;
+        this.salesPrice = product.salesPrice;
+        this.cost = product.cost;
+        this.quantity = product.quantity;
+        this.active = product.active;
+    }
+
     float getProfitOrLoss() {
-        return salesPrice - cost;
+        return this.salesPrice - this.cost;
     }
 
     void activate() {
-        active = true;
+        this.active = true;
     }
 
     void deactivate() {
-        active = false;
+        this.active = false;
     }
 
     boolean isBelowThreshold() {
-        return quantity < 100;
+        return this.quantity < 100;
     }
 }
