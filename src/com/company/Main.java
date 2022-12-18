da6c770985d0d593cf69d595ee49e33d5996c204
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-12-18 12:54:35 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,12 +10,12 @@ public class Main {
 
         Customer customer =
                 new Customer("2nd street", "Bengaluru",
-                        "Karnataka", "Srishti Gupta",
-                        "9090909090", "srishti.gupta@gmail.com", 10);
+                        "Karnataka", "a singh",
+                        "9090909090", "1@gmail.com", 10);
 
         Vendor lenVenInfoHub = new Vendor("8th street", "Bengaluru",
-                "Karnataka", "Ishwar", "1234567890",
-                "ishwar.soni@gmail.com", "LenVen InfoHub");
+                "Karnataka", "b singh", "1234567890",
+                "B @gmail.com", "LenVen InfoHub");
 
         Order order1 = new Order(1, lenVenInfoHub, "26/10/2020", appleMacBookAir,
                 10, 10000);
